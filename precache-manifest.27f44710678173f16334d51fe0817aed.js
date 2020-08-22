e5d5d6b25612f4b59912ecd1d7bc2351cf2d3d2a
Aleksey <alexsawenkov@gmail.com>
2020-08-22 04:52:11 +0300

    TestVK

---
+++ precache-manifest.27f44710678173f16334d51fe0817aed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ad2b4ab6c386ce9c5048add573b0f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6723178be004c801f2c4",
+    "url": "/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "f710f9b01ba2d2786c63",
+    "url": "/static/css/main.5e745eaf.chunk.css"
+  },
+  {
+    "revision": "6723178be004c801f2c4",
+    "url": "/static/js/2.ad12ee5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad12ee5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f710f9b01ba2d2786c63",
+    "url": "/static/js/main.d4335862.chunk.js"
+  },
+  {
+    "revision": "6e6a49ec5e36c73a89cc",
+    "url": "/static/js/runtime-main.e6c9cfb6.js"
+  }
+]);
